a56d16f7a3294d044c969578dddfe2b3e2b35f76
seunghyeokleeme <seunghyeok.tech@gmail.com>
2025-01-01 18:44:03 +0900

    feat: deque

---
+++ deque/main.cpp
@@ -0,0 +1,33 @@
+#include "../utils/Deque.h"
+
+#include <iostream>
+
+int main()
+{
+    // Double Ended Queue (덱)
+    Deque<char> d(8);
+
+    d.SetDebugFlag(true);
+
+    d.Print();
+
+    d.PushFront('A');
+    d.Print();
+
+    d.PushFront('B');
+    d.Print();
+
+    d.PushBack('C');
+    d.Print();
+
+    d.PushBack('D');
+    d.Print();
+
+    d.PopFront();
+    d.Print();
+
+    d.PopBack();
+    d.Print();
+
+    return 0;
+}
